7859fb686d4cb68e0229ea0f9c467a5007a9575d
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-04-06 20:54:40 -0300

    weartake to kenzie burguer

--- src/components/body/Projects/Projects.jsx
+++ src/components/body/Projects/Projects.jsx
@@ -2,7 +2,7 @@ import { StyledProjects } from "./style"
 import kenziehub from "../../../assets/img/projects/kenzie hub.png"
 import nukenzie from "../../../assets/img/projects/nu kenzie.png"
 import pokedex from "../../../assets/img/projects/pokedex.jpg"
-import weartake from "../../../assets/img/projects/weartake.png"
+import kenziburguer from "../../../assets/img/projects/kenzie burguer.png"
 import { ProjectCard } from "../../cards/ProjectCard/ProjectCard"
 
 
@@ -16,7 +16,7 @@ export const Projects = () => {
                 <ProjectCard logo={kenziehub} text="Kenzie Hub" link="https://s3-12.vercel.app"/>
                 <ProjectCard logo={nukenzie} text="Nu Kenzie" link="https://nu-kenzie-lac.vercel.app"/>
                 <ProjectCard logo={pokedex} text="Pokedex" link="https://pokedex-nextjs-sage-three.vercel.app"/>
-                <ProjectCard logo={weartake} text="Weartake" link="https://rafafreitasdev.github.io/weartake/"/> 
+                <ProjectCard logo={kenziburguer} text="Kenzie Burguer" link="https://s2-16.vercel.app"/> 
             </div>
                 
         </StyledProjects>
